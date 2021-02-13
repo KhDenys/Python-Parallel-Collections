a9e558774032a10fa8058c9e4df8f7f0f196291d
KhDenys <KhD01214@outlook.com>
2021-02-13 19:33:28 +0200

    added some benchmarks

---
+++ bench/bench.py
@@ -0,0 +1,74 @@
+from math import sqrt
+from multiprocessing import Pool
+from timeit import Timer
+
+from ..parallel import parallel
+
+
+def get_divisors_sum(n):
+    res = 1
+    for i in range(2, int(sqrt(n)) + 1):
+        if not n % i:
+            res += i
+            j = n // i
+            if j > i:
+                res += j
+
+    return res
+
+
+def is_prime(m):
+    if m == 1:
+        return False
+    for i in range(2, int(sqrt(m)) + 1):
+        if not m % i:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+
+    def test_multiprocessing_pool(n):
+        with Pool(processes=4) as pool:
+            return pool.map(get_divisors_sum, range(n))
+
+    def test_parallel_map(n):
+        *l, = parallel(range(n), pool_size=4).map(get_divisors_sum)
+        return l
+
+    def test_map(n):
+        *l, = map(get_divisors_sum, range(n))
+        return l
+
+    def test_parallel_filter(n):
+        *l, = parallel(range(n), pool_size=4).filter(is_prime)
+        return l
+
+    def test_filter(n):
+        *l, = filter(is_prime, range(n))
+        return l
+
+    # equality check
+    a = 1000
+    assert test_multiprocessing_pool(a) == test_parallel_map(a) == test_map(a)
+    assert test_parallel_filter(a) == test_filter(a)
+
+    # performance check
+    b = 1_000_000
+    loops = 3
+
+    test_multiprocessing_pool_timer = Timer(lambda: test_multiprocessing_pool(b))
+    test_parallel_map_timer = Timer(lambda: test_parallel_map(b))
+    test_map_timer = Timer(lambda: test_map(b))
+
+    print('test_multiprocessing_pool: ', test_multiprocessing_pool_timer.timeit(loops) / loops)
+    print('test_map: ', test_map_timer.timeit(loops) / loops)
+    print('test_parallel_map: ', test_parallel_map_timer.timeit(loops) / loops)
+
+    print()
+
+    test_parallel_filter_timer = Timer(lambda: test_parallel_filter(b))
+    test_filter_timer = Timer(lambda: test_filter(b))
+
+    print('test_filter: ', test_filter_timer.timeit(loops) / loops)
+    print('test_parallel_filter: ', test_parallel_filter_timer.timeit(loops) / loops)
